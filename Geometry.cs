72a99fffaeada0870b91a17e667ac00bc48e3cbc
DanielNilsson-GIT <daniel.gronberg92@gmail.com>
2025-10-31 10:43:28 +0100

    Add project files.

---
+++ Geometry.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labb_3_polymorphism_Daniel_Nilsson_SUT25
+{
+    internal class Geometry
+    {
+        
+        public double Area()
+        {
+
+            return 
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Labb_3_polymorphism_Daniel_Nilsson_SUT25
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
