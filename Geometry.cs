65e271c6bd1965ca55ab0ee217a341168b10fe26
DanielNilsson-GIT <daniel.gronberg92@gmail.com>
2025-11-01 13:32:19 +0100

    Experimented with acces-modifiers to see what they do. This is why "Type" is protected and read-only

--- Geometry.cs
+++ Geometry.cs
@@ -8,7 +8,7 @@ namespace Labb_3_polymorphism_Daniel_Nilsson_SUT25
 {
     internal class Geometry
     {
-        protected virtual string Type { get; }//Experimented with accesmodifiers to see what they do. 
+        protected virtual string Type { get; }//Experimented with acces-modifiers to see what they do. 
 
         public virtual double Area()
         {
